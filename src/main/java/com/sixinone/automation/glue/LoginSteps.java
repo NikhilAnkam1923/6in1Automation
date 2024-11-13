8e8806dfa6870b8109462342bf36d8754be59589
GauravGidye26 <gauravgidye26@gmail.com>
2024-11-13 18:59:42 +0530

    Added Test Cases for verifying error messages

--- src/main/java/com/sixinone/automation/glue/LoginSteps.java
+++ src/main/java/com/sixinone/automation/glue/LoginSteps.java
@@ -1,11 +1,16 @@
 package com.sixinone.automation.glue;
 
 import com.sixinone.automation.exception.AutomationException;
+import com.sixinone.automation.pages.LoginPage;
 import com.sixinone.automation.pages.PageFactory;
 import com.sixinone.automation.util.CommonUtil;
+import com.sixinone.automation.util.WebDriverUtil;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import static com.sixinone.automation.pages.BasePage.driverUtil;
 
 public class LoginSteps {
     @Then("^User login with \"([^\"]*)\" and \"([^\"]*)\"$")
@@ -31,6 +36,7 @@ public class LoginSteps {
         password = CommonUtil.processString(password);
         CommonSteps.logInfo("User login with user: " + userEmail + " and password: *********");
         PageFactory.loginPage().loginTo6in1(userEmail, password);
+
     }
 
     @Given("user verify login page ui attributes")
@@ -52,10 +58,23 @@ public class LoginSteps {
         PageFactory.loginPage().doLogoutFrom6in1();
     }
 
-    @When("^user verify \"([^\"]*)\" for invalid credentials$")
+    @When("^user verify \"([^\"]*)\"$")
     public void userVerifyInvalidCredErrorMessage(String errorMessage) throws AutomationException {
-        CommonSteps.logInfo("User verify error message for invalid credentials");
+        CommonSteps.logInfo("User verify error message");
         PageFactory.loginPage().verifyInvalidCredErrorMessage(errorMessage);
         CommonSteps.takeScreenshot();
+        driverUtil.getWebElementAndScroll(LoginPage.USER_EMAIL_INPUT).clear();
+    }
+
+    @When("^user enter \"([^\"]*)\"$")
+    public void userEnterPassword(String password) throws AutomationException {
+        CommonSteps.logInfo("user enter password: *********");
+        PageFactory.loginPage().enterPassword(password);
+    }
+
+    @When("^user login using \"([^\"]*)\"$")
+    public void userLoginUsing(String email) throws AutomationException {
+        PageFactory.loginPage().enterUserEmail(email);
+        PageFactory.loginPage().clickLoginButton();
     }
 }
