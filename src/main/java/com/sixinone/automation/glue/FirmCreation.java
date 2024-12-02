37e7c07a5e5263f7621749d9d2459d40e84fb8a6
GauravGidye26 <gauravgidye26@gmail.com>
2024-12-02 19:53:31 +0530

    added test case to verify successful firm addition

--- src/main/java/com/sixinone/automation/glue/FirmCreation.java
+++ src/main/java/com/sixinone/automation/glue/FirmCreation.java
@@ -3,6 +3,7 @@ package com.sixinone.automation.glue;
 import com.sixinone.automation.exception.AutomationException;
 import com.sixinone.automation.pages.PageFactory;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 
 public class FirmCreation {
 
@@ -33,5 +34,78 @@ public class FirmCreation {
         CommonSteps.logInfo("Breadcrumb validation successful: 'Firm > New' is correctly displayed.");
         CommonSteps.takeScreenshot();
     }
+
+    @Then("^user enter \"([^\"]*)\" in \"([^\"]*)\" Field$")
+    public void userEnterInField(String fieldData, String fieldName) throws AutomationException {
+        CommonSteps.logInfo("User enters '" + fieldData + "' in the field '" + fieldName + "'.");
+
+        boolean isInputSuccessful = PageFactory.firmCreationPage().enterDataInFieldByLabel(fieldData, fieldName);
+
+        if (!isInputSuccessful) {
+            throw new AutomationException("Failed to enter data in field with label: '" + fieldName + "'.");
+        }
+
+        CommonSteps.logInfo("Successfully entered data in the field: " + fieldName);
+        CommonSteps.takeScreenshot();
+
+    }
+
+    @And("^user enter phone \"([^\"]*)\" for Firm$")
+    public void userEnterPhoneOfFirm(String phone) throws AutomationException {
+        CommonSteps.logInfo("User enters the phone '" + phone + "' for the firm.");
+
+        boolean isInputSuccessful = PageFactory.firmCreationPage().enterFirmPhone(phone);
+
+        if (!isInputSuccessful) {
+            throw new AutomationException("Failed to enter phone for the firm.");
+        }
+
+        CommonSteps.logInfo("Successfully entered phone for the firm: " + phone);
+        CommonSteps.takeScreenshot();
+    }
+
+
+    @And("^user enter phone \"([^\"]*)\" for Primary User$")
+    public void userEnterPhoneOfUser(String phone) throws AutomationException {
+        CommonSteps.logInfo("User enters the phone '" + phone + "' for the Primary User.");
+
+        boolean isInputSuccessful = PageFactory.firmCreationPage().enterUserPhone(phone);
+
+        if (!isInputSuccessful) {
+            throw new AutomationException("Failed to enter phone for the Primary User.");
+        }
+
+        CommonSteps.logInfo("Successfully entered phone for the Primary User: " + phone);
+        CommonSteps.takeScreenshot();
+    }
+
+    @And("^user select \"([^\"]*)\" as \"([^\"]*)\"$")
+    public void userSelectAsLicenseType(String labelName, String option) throws AutomationException {
+        CommonSteps.logInfo("User selects '" + option + "' from the '" + labelName + "' dropdown.");
+
+        boolean isSelectionSuccessful = PageFactory.firmCreationPage().selectOption(labelName, option);
+
+        if (!isSelectionSuccessful) {
+            throw new AutomationException("Failed to select '" + option + "' from the '" + labelName + "' dropdown.");
+        }
+
+        CommonSteps.logInfo("Successfully selected '" + option + "' from the '" + labelName + "' dropdown.");
+        CommonSteps.takeScreenshot();
+    }
+
+    @Then("^Verify the success message \"([^\"]*)\"$")
+    public void verifySuccessMessage(String expectedMessage) throws AutomationException {
+        CommonSteps.logInfo("Verifying the success message: '" + expectedMessage + "'.");
+
+        boolean isMessageDisplayed = PageFactory.firmCreationPage().verifySuccessMessage(expectedMessage);
+
+        if (!isMessageDisplayed) {
+            throw new AutomationException("Expected success message '" + expectedMessage + "' was not displayed.");
+        }
+
+        CommonSteps.logInfo("Successfully verified the success message: " + expectedMessage);
+        CommonSteps.takeScreenshot();
+    }
+
 
 }
--- src/main/java/com/sixinone/automation/pages/FirmCreationPage.java
+++ src/main/java/com/sixinone/automation/pages/FirmCreationPage.java
@@ -5,13 +5,13 @@ import com.sixinone.automation.glue.CommonSteps;
 import com.sixinone.automation.util.WebDriverUtil;
 import org.openqa.selenium.WebElement;
 
-import java.util.logging.Logger;
 
 public class FirmCreationPage {
 
     public static WebDriverUtil driverUtil = new WebDriverUtil();
+    public static final String FIELD_XPATH = "//label[text()='%s']/following-sibling::input";
 
-    public boolean isFirmBreadcrumbVisible() throws AutomationException {
+    public boolean isFirmBreadcrumbVisible() {
         WebDriverUtil.waitForElementNotVisible(60, LoginPage.SPINNER);
         String breadcrumbLocator = "//nav[@aria-label='breadcrumb']//ol//li[@class='breadcrumb-item']//a[text()='Firm']";
 
@@ -35,4 +35,99 @@ public class FirmCreationPage {
             return false;
         }
     }
+
+    public boolean enterDataInFieldByLabel(String fieldData, String fieldName){
+        String inputFieldXPath = String.format(FIELD_XPATH, fieldName);
+
+        try {
+            WebElement inputField = driverUtil.getWebElement(inputFieldXPath, 5);
+            if (inputField == null) {
+                throw new AutomationException("Input field with label '" + fieldName + "' not found.");
+            }
+
+            inputField.clear();
+            inputField.sendKeys(fieldData);
+            return true;
+        } catch (Exception e) {
+            CommonSteps.logError("Error entering data in field with label '" + fieldName + "': " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean enterFirmPhone(String data){
+        try {
+            String phoneFieldXPath = "//input[@name='lawFirm.phone']";
+            WebElement PHONE_FIRM = driverUtil.getWebElement(phoneFieldXPath, 5);
+
+            if (PHONE_FIRM == null) {
+                throw new AutomationException("Firm phone input field not found.");
+            }
+
+            PHONE_FIRM.clear();
+
+            PHONE_FIRM.sendKeys(data);
+
+            return true;
+        } catch (Exception e) {
+            CommonSteps.logError("Error entering firm phone: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean enterUserPhone(String data){
+        try {
+            String phoneFieldXPath = "//input[@name='accountOwner.phone']";
+            WebElement PHONE_USER = driverUtil.getWebElement(phoneFieldXPath, 5);
+
+            if (PHONE_USER == null) {
+                throw new AutomationException("Primary User phone input field not found.");
+            }
+
+            PHONE_USER.clear();
+
+            PHONE_USER.sendKeys(data);
+
+            return true;
+        } catch (Exception e) {
+            CommonSteps.logError("Error entering firm phone: " + e.getMessage());
+            return false;
+        }
+    }
+
+
+
+    public boolean selectOption(String labelName, String option){
+        try {
+            WebElement comboBox = driverUtil.getWebElement(String.format("//label[text()='%s']/following-sibling::div/span/following-sibling::div//div[contains(@class,'select__indicators')]", labelName), 5);
+
+            comboBox.click();
+
+            WebElement selectOption = driverUtil.getWebElement(String.format("//div[contains(@class,'select__menu-list')]//div[text()='%s']", option), 5);
+
+            selectOption.click();
+
+            return true;
+        } catch (Exception e) {
+            CommonSteps.logError("Failed to select license type: " + option);
+            return false;
+        }
+    }
+
+    public boolean verifySuccessMessage(String expectedMessage){
+        try {
+            WebElement toastMessageElement = driverUtil.getWebElement("//div[@class='Toastify__toast-body']//div/following-sibling::div[contains(text(),'" + expectedMessage + "')]", 10);
+
+            if (toastMessageElement != null && toastMessageElement.isDisplayed()) {
+                String actualMessage = toastMessageElement.getText();
+                return actualMessage.equals(expectedMessage);
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            CommonSteps.logError("Failed to verify the success message: " + expectedMessage);
+            return false;
+        }
+    }
+
+
 }
