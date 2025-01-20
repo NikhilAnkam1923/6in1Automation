709ad6a2112a84d9a9b70938b490c05b73f9d81b
GauravGidye26 <gauravgidye26@gmail.com>
2025-01-20 19:23:14 +0530

    added 3 TCs for RW03 probate form

--- features/probateFormsRW03.feature
+++ features/probateFormsRW03.feature
@@ -11,15 +11,28 @@ Feature: 6in1 probate form RW03 Feature
       | user-email                           | password |
       | nikhilankam+14@benchmarkit.solutions | Watch@22 |
 
-Scenario: Verify county, estate and aka names are autopopulated on the form
-  When user select estate and retrieve the entered county, estate and aka names
-  Then user navigates to the probate forms tab
-  And user click on the RW03
-  Then user selects the aka checkbox
-  And user verifies the county, estate and aka names are autopopulated on the form
+  Scenario: User Creates an Estate
+    When user clicks on Create button
+    Then user fills first name,last name and SSN details
+    And user clicks on Proceed button
+    And user click on Create a new estate with the entered name button
+    Then user fills decedent basic information
+    And user clicks on Next button
+    When user fills Last Address/Domicile details
+    When user fills Life Details
+    When user fills the Place of Death details
+    And user click on Estate tab
+    And user fills the Estate details
 
-
-
+  Scenario: Verify county, estate and aka names are auto-populated on the form
+    When user navigates to the probate forms tab
+    And user click on the "RW 03" form
+    Then user selects the aka checkbox
+    And user verifies the county, estate and aka names are auto-populated on the form
+     #Verify, the auto-populated fields are not editable.
+    Then user verifies the auto-populated fields are not editable
+     #Verify, witness's name is not auto populated and the fields are empty.
+    Then user verifies witness's name is not auto populated and the fields are empty
 
 
 
--- src/main/java/com/sixinone/automation/glue/ProbateFormsRW03Steps.java
+++ src/main/java/com/sixinone/automation/glue/ProbateFormsRW03Steps.java
@@ -2,24 +2,122 @@ package com.sixinone.automation.glue;
 
 import com.sixinone.automation.exception.AutomationException;
 import com.sixinone.automation.pages.PageFactory;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import gherkin.lexer.Pa;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 import static com.sixinone.automation.util.WebDriverUtil.waitForInvisibleElement;
 import static com.sixinone.automation.util.WebDriverUtil.waitForVisibleElement;
 
 public class ProbateFormsRW03Steps {
 
+    @When("user clicks on Create button")
+    public void userClicksOnTheCreateButton() throws AutomationException {
+        CommonSteps.logInfo("User clicks on the Create button");
+        PageFactory.probateFormsRW03Page().clickButtonCreate();
+    }
+
+    @Then("user fills first name,last name and SSN details")
+    public void userFillsTheFirstNameLastNameAndSSNDetails() throws AutomationException, IOException, ParseException {
+        CommonSteps.logInfo("User filled the first name, last name, and SSN details.");
+        PageFactory.probateFormsRW03Page().enterFirstAndLastNameAndSSN();
+    }
+
+    @And("user clicks on Proceed button")
+    public void userClickOnProceedButton() throws AutomationException {
+        CommonSteps.logInfo("User clicked on the Proceed button.");
+        PageFactory.probateFormsRW03Page().clickOnProceedButton();
+    }
+
+    @And("^user click on Create a new estate with the entered name button$")
+    public void userClickCreateNewEstateButton() throws AutomationException {
+        CommonSteps.logInfo("User clicked on 'Create a new estate with the entered name' button for new user.");
+        PageFactory.probateFormsRW03Page().clickCreateNewEstateButton();
+    }
+
+    @Then("^user fills decedent basic information$")
+    public void userFillsDecedentBasicInformation() throws AutomationException, IOException, ParseException {
+        CommonSteps.logInfo("User filled decedent basic information for new user.");
+        PageFactory.probateFormsRW03Page().fillDecedentBasicInformation();
+    }
+
+    @And("user clicks on Next button")
+    public void userClickOnNextButton() throws AutomationException {
+        CommonSteps.logInfo("User clicked on the Next button.");
+        PageFactory.probateFormsRW03Page().clickOnNextButton();
+    }
+
+    @When("^user fills Last Address\\/Domicile details$")
+    public void userFillLastAddressDomicileDetails() throws AutomationException, IOException, ParseException {
+        CommonSteps.logInfo("User filled Last Address/Domicile details.");
+        PageFactory.probateFormsRW03Page().fillLastAddressDomicileDetails();
+    }
+
+    @When("user fills Life Details")
+    public void userFillsLifeDetails() throws AutomationException, IOException, ParseException {
+        CommonSteps.logInfo("user fills Life Details and validate the fields");
+        PageFactory.probateFormsRW03Page().fillLifeDetails();
+    }
+
+    @When("user fills the Place of Death details")
+    public void userFillsPlaceOfDeathDetails() throws AutomationException, IOException, ParseException {
+        CommonSteps.logInfo("User filled Place of Death details.");
+        PageFactory.probateFormsRW03Page().fillPlaceOfDeathDetails();
+    }
+
+    @When("user click on Estate tab")
+    public void userClicksOnEstateTab() throws AutomationException {
+        CommonSteps.logInfo("User clicked on the Estate tab.");
+        PageFactory.probateFormsRW03Page().clickOnEstateTab();
+    }
+
+    @And("user fills the Estate details")
+    public void userFillsEstateDetails() throws AutomationException, IOException, ParseException {
+        CommonSteps.logInfo("User filled Estate details.");
+        PageFactory.probateFormsRW03Page().fillEstateDetails();
+    }
+
     @When("user navigates to the probate forms tab")
     public void userNavigatesToTheProbateFormsTab() throws AutomationException {
+        CommonSteps.logInfo("user navigates to the probate forms tab");
         PageFactory.probateFormsRW03Page().navigateToEstateContactsTab();
     }
 
-    @Then("user click on the RW{int}")
-    public void userClickOnTheRW(int arg0) {
+
+    @Then("^user click on the \"([^\"]*)\" form$")
+    public void userClickOnTheRW(String formToSelect) throws AutomationException {
+        CommonSteps.logInfo("user click on the "+formToSelect+" form");
+        PageFactory.probateFormsRW03Page().clickOnRWForm(formToSelect);
     }
 
-    @When("user select estate and retrieve the entered county, estate and aka names")
-    public void userSelectEstateAndRetrieveTheEnteredCountyEstateAndAkaNames() {
+    @Then("user selects the aka checkbox")
+    public void userSelectsTheAkaCheckbox() throws AutomationException {
+        CommonSteps.logInfo("user selects the aka checkbox");
+        PageFactory.probateFormsRW03Page().selectAKACheckbox();
+    }
+
+    @And("user verifies the county, estate and aka names are auto-populated on the form")
+    public void userVerifiesTheCountyEstateAndAkaNamesAreAutoPopulatedOnTheForm() throws AutomationException {
+        PageFactory.probateFormsRW03Page().verifyCountyEstateAndAkaNamesAreAutoPopulatedOnTheForm();
+        CommonSteps.logInfo("Verified that the county, estate and aka names are auto-populated on the form");
+        CommonSteps.takeScreenshot();
+    }
+
+    @Then("user verifies the auto-populated fields are not editable")
+    public void userVerifiesTheAutoPopulatedFieldsAreNotEditable() throws Exception {
+        CommonSteps.logInfo("Verified that the auto-populated fields are not editable");
+        PageFactory.probateFormsRW03Page().verifyAutoPopulatedFieldsAreNotEditable();
+        CommonSteps.takeScreenshot();
+    }
+
+    @Then("user verifies witness's name is not auto populated and the fields are empty")
+    public void userVerifiesWitnessesSNameIsNotAutoPopulatedAndTheFieldsAreEmpty() throws Exception {
+        CommonSteps.logInfo("Verified that witness's name is not auto populated and the fields are empty");
+        PageFactory.probateFormsRW03Page().verifyWitnessesSNameIsNotAutoPopulatedAndTheFieldsAreEmpty();
+        CommonSteps.takeScreenshot();
     }
 }
