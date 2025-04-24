129213825dfeab7d49f26ccad8925fdc62d29f9b
NikhilAnkam1923 <nikhilankam@benchmarkit.solutions>
2025-04-24 16:14:02 +0530

    continue with OC02 TCs

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 @CucumberOptions(
 
 
-        features = {"features/probateFormsRW06.feature"},
+        features = {"features/probateFormsOC02.feature"},
         glue = { "com.sixinone.automation.glue"},
         monochrome = true
 )
